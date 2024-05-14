3335ee60ce7e4a2497c41e58b8304e345a9502e9
Olivelton Santos <124373744+oliveltonsantos@users.noreply.github.com>
2024-05-14 11:08:34 -0300

    update

---
+++ aula-14-exercicios/ex016/script.js
@@ -0,0 +1,9 @@
+function contar() {
+    var ninicial = window.document.getElementById('ninicial')
+    var nfinal = window.document.getElementById('nfinal')
+    var passo = window.document.getElementById('passo')
+    var res = window.document.getElementById('res')
+    if (passo.value.length == 0) {
+        window.alert ('Passo inválido! Considerando PASSO com valor igual a 1.')
+    }
+}
