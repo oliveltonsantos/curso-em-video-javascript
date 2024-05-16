565c65967391dabcaf35569551316bcdc11e5109
Olivelton Santos <124373744+oliveltonsantos@users.noreply.github.com>
2024-05-16 13:14:27 -0300

    update

--- aula-14-exercicios/ex016/script.js
+++ aula-14-exercicios/ex016/script.js
@@ -1,10 +1,37 @@
+/* 
+
+lenght: diz respeito ao comprimento, ou seja, quantas letras tem dentro do campo.
+
+O código unicode dos emojis só funciona entre crase `\u{1F449}` 
+
+*/
+
 function contar() {
-    var ninicial = window.document.getElementById('ninicial')
-    var nfinal = window.document.getElementById('nfinal')
-    var passo = window.document.getElementById('passo')
-    var res = window.document.getElementById('res')
-    if (passo.value.length == 0) {
-        window.alert('Passo inválido! Considerando PASSO com valor igual a 1.')
-    } else (ninicial.value.length == 0 || nfinal.value.length == 0) 
-    window.alert('Impossível contar!')
+    let ini = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = 'Impossível contar!'
+        window.alert('[ERRO!] Faltam dados!')
+    } else {
+        res.innerHTML = 'Contando: '
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+
+        if (i < f) {
+            //Contagem crescente
+            for (let c = i; c <=f; c += p ) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        } else {
+            //Contagem regressiva
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{1f449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1} FIM!`
+    }
 }
