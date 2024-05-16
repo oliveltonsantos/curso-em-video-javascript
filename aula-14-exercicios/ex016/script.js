399a44462dcb1dbc318d878cf3eaae7480bc753f
Olivelton Santos <124373744+oliveltonsantos@users.noreply.github.com>
2024-05-16 19:14:07 -0300

    update

--- aula-14-exercicios/ex016/script.js
+++ aula-14-exercicios/ex016/script.js
@@ -13,21 +13,24 @@ function contar() {
     let res = document.getElementById('res')
 
     if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        res.innerHTML = 'Impossível contar!'
-        window.alert('[ERRO!] Faltam dados!')
+        res.innerHTML = 'Impossível contar! Digite um número em cada um dos campos acima.'
     } else {
-        res.innerHTML = 'Contando: '
+        res.innerHTML = 'Contando: <br>'
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
 
+        if (p <= 0) {
+            window.alert('Passo inválido! Considerando o valor de PASSO igual a 1.')
+            p = 1
+        }
         if (i < f) {
-            //Contagem crescente
-            for (let c = i; c <=f; c += p ) {
+            // Contagem crescente
+            for (let c = i; c <= f; c += p) {
                 res.innerHTML += `${c} \u{1F449}`
             }
         } else {
-            //Contagem regressiva
+            // Contagem regressiva
             for (let c = i; c >= f; c -= p) {
                 res.innerHTML += `${c} \u{1f449}`
             }
