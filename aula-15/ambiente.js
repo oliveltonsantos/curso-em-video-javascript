64d1a971407b41425046e822d9bdcefb3193ffe8
Olivelton Santos <124373744+oliveltonsantos@users.noreply.github.com>
2024-05-21 12:31:08 -0300

    update

---
+++ aula-15/ambiente.js
@@ -0,0 +1,17 @@
+let num = [5, 8, 2, 9, 3]
+num.push(1)
+num.sort()
+console.log(num)
+console.log(`Nosso vetor tem ${num.length} posições.`)
+console.log(`O primeiro valor é ${num[0]}.`)
+
+/*Uso do indexOf*/
+
+let pos = num.indexOf(5)
+
+if (pos == -1) {
+    console.log('O valor não foi encontrado!')
+} else {
+    console.log(`O valor está na posição ${pos}.`)
+}
+
