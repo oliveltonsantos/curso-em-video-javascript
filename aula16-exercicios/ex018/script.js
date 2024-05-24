5019f7f160e7d136195bd9a82099789e08bdcaa1
Olivelton Santos <124373744+oliveltonsantos@users.noreply.github.com>
2024-05-24 19:43:50 -0300

    update

--- aula16-exercicios/ex018/script.js
+++ aula16-exercicios/ex018/script.js
@@ -3,14 +3,6 @@ let lista = document.getElementById('flista')
 let res = document.getElementById('res')
 let valores = [] // Variável composta
 
-function adicionar() {
-    if (isNumero(num.value) && !inLista(num.value, valores)) {
-
-    } else {
-        window.alert('Valor inválido ou já encontrado na lista.')
-    }
-}
-
 // Verifica se a caixa está vazia ou se o valor está entre 1 e 100.
 
 function isNumero(n) {
@@ -30,4 +22,33 @@ function inLista(n, l) {
         return false
     }
 }
+
+function adicionar() {
+    if (isNumero(num.value) && !inLista(num.value, valores)) {
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
+        res.innerHTML = ''  // se a pessoa adicionar novos números as respostas anteriores serão apagadas
+    } else {
+        window.alert('Valor inválido ou já encontrado na lista.')
+    }
+    num.value = '' // apagar o número já adicionado do campo
+    num.focus() // ponteiro do campo ficar piscando
+}
+
+
+function finalizar() {
+    if (valores.length == 0) {
+        window.alert('Adicione valores antes de finalizar!')
+    } else {
+
+        let tot = valores.length // saber o total de elementos
+
+
+        res.innerHTML = '' // zerar o valor de res
+        res.innerHTML += `Ao todo, temos ${tot} números cadastrados.`
+    }
+}
+
 
