b345a66061a0a38d182b89685706938dfca5de97
Olivelton Santos <124373744+oliveltonsantos@users.noreply.github.com>
2024-07-09 19:26:18 -0300

    update

---
+++ curso-js/a.js
@@ -0,0 +1,4 @@
+let passouEnem = false
+let passouVestibular = true
+let entrouFaculdade = (passouEnem == true || passouVestibular== true)
+console.log("Entrou na faculdade? " + entrouFaculdade)
--- curso-js/aula-7.js
+++ curso-js/aula-7.js
@@ -1,3 +1,5 @@
+/* 
+
 let nome1 = "Ana"
 let nome2 = "Paulo"
 let nomeIgual = nome1 == nome2
@@ -11,3 +13,28 @@ console.log(comparacaoDois)
 let lista = ['elemento A', 'elemento B']
 let inclui = lista.includes('elemento C')
 console.log(inclui)
+
+
+let nota1 = 40
+let nota2 = 70
+
+let passouAno = (nota1 > 60 && nota2 > 60)
+console.log("O aluno passou de ano? " + passouAno)
+
+let nome = "João"
+let sobrenome = "Silva"
+let eJoaoCarlos = (nome == "João" && sobrenome == "Carlos")
+console.log("Esse nome é João Carlos? " + eJoaoCarlos)
+
+*/
+
+let passouEnem = false
+let passouVestibular = true
+let entrouFaculdade = (passouEnem == true || passouVestibular == true)
+console.log("Entrou na faculdade? " + entrouFaculdade)
+
+
+let passouMatematica = true
+passouMatematica = !passouMatematica
+console.log(passouMatematica)
+
